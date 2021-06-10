109f6c8cb24f6f9069cefdf161cff7a4ff9781d6
Hunter MacDonald <mactinite77@gmail.com>
2021-06-09 23:49:15 -0700

    Add a  component that fires a unity event when damage is taken

---
+++ Runtime/DamageEvent.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using mactinite.DamageReceiver;
+using mactinite.ToolboxCommons;
+namespace mactinite.DamageReceiver
+{
+    public class DamageEvent : MonoBehaviour
+    {
+        public FloatUnityEvent OnDamage = new FloatUnityEvent();
+
+
+        private void Start()
+        {
+            GetComponent<DamageReceiver>().OnDamage += OnDamageReceived;
+        }
+
+        private void OnDamageReceived(Vector2 pos, Damage dmg)
+        {
+            OnDamage.Invoke(dmg.damageAmount);
+        }
+
+    }
+}
